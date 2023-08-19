b06aaa0332c3dea857b68fad4c9f42a6c8ed7c98
sonikumari1231234 <142649272+sonikumari1231234@users.noreply.github.com>
2023-08-19 10:55:58 +0530

    Add files via upload

---
+++ area of circle NPTL.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int radious;
+ float area ;
+  float pi=3.14;
+ printf("Enter number");
+ scanf("%d",radious);
+ area=pi*radious*radious;
+ printf("%d area is.",area);
+}
+
